fab2ae54e5b43857545c0c086916eaeb4a7bf90b
Frank Edwards <frankedwards50@gmail.com>
2025-04-26 00:51:57 -0700

    Config for Prod revised

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -1,10 +1,50 @@
-export default {
-    expo: {
-      name: "Parate Porfavor",
-      slug: "your-app-slug",
-      extra: {
-        encryptionSecret: process.env.EXPO_PUBLIC_ENCRYPTION_SECRET || "fallback",
-      }
+import { ExpoConfig, ConfigContext } from "@expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Parate Porfavor",
+  slug: "parate-porfavor",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/images/icon.png",
+    resizeMode: "cover",
+    backgroundColor: "#040A15"
+  },
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    package: "com.ezekias1337.parateporfavor", // REQUIRED for Play Store
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/icon.png",
+      backgroundColor: "#040A15"
     }
-  };
-  
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    "expo-router",
+    "expo-font",
+    "expo-localization"
+  ],
+  experiments: {
+    typedRoutes: true
+  },
+  androidStatusBar: {
+    barStyle: "light-content",
+    backgroundColor: "#4276CF"
+  },
+  extra: {
+    encryptionSecret: process.env.EXPO_PUBLIC_ENCRYPTION_SECRET || "fallback",
+    eas: {
+      projectId: "44357fa5-05e3-4415-9214-99edeebb023f"
+    }
+  }
+});
